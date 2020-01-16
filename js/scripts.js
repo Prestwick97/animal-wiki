5ce6d80e6824d89f1502b5c2d24b94bf15b6d3b4
Patrick Delaney and Tiffany Nguyen <tiffany@>
2020-01-16 14:55:56 -0800

    created new html

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var jd = ("#depp")
+
+  if ("#ro" && "#or" || "#po" && "#as") {
+    $("#depp").show();
+  // } else if (age === 21) {
+  //   alert("Now don't go crazy!");
+  //   $('#drinks').show();
+  }
+
+});
