3771a5713b5c013a6f91f992edf4859fc725c07e
Patrick Delaney and Tiffany Nguyen <patrick@>
2020-01-16 16:51:49 -0800

    add interactive submit button for dating site

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-  var jd = ("#depp")
+  $("form#celebrity").submit(function(event) {
 
-  if ("#ro" && "#or" || "#po" && "#as") {
-    $("#depp").show();
-  // } else if (age === 21) {
-  //   alert("Now don't go crazy!");
-  //   $('#drinks').show();
+    var beverage = $("#beverage").val();
+    console.log(beverage);
+  var food = $("#food").val();
+  console.log(food);
+
+  if (beverage === "1" && food === "a") {
+    $("#jd").show();
+    // var js = "#jd"
   }
 
+  event.preventDefault();
+  });
 });
